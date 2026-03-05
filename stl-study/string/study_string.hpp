4d42baee4c94d3a9d85936d831767d3e8d636abf
power12562 <kimsiwoo1005@gmail.com>
2026-03-05 17:34:41 +0900

    wchar_t traits 추가

--- stl-study/string/study_string.hpp
+++ stl-study/string/study_string.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <string.h>
+#include <cwchar>
 
 namespace mst
 {
@@ -22,17 +23,47 @@ namespace mst
 
 		static void copy(char_t* dst, const char_t* src, size_t dstSize)
 		{
-			memcpy(dst, src, sizeof(char_t) * dstSize);
+			memcpy(dst, src, dstSize);
 		}
 
 		static void move(char_t* dst, const char_t* src, size_t dstSize)
 		{
-			memmove(dst, src, sizeof(char_t) * dstSize);
+			memmove(dst, src, dstSize);
 		}
 
 		static void assing(char_t* dst, char_t c, size_t count)
 		{
 			memset(dst, c, count);		
+		}
+	};
+
+	template<>
+	struct char_traits<wchar_t>
+	{
+		using char_t = wchar_t;
+		static size_t length(const char_t* str)
+		{
+			return wcslen(str);
+		}
+
+		static int compare(const char_t* str1, const char_t* str2, size_t count)
+		{
+			return wmemcmp(str1, str2, count);
+		}
+
+		static void copy(char_t* dst, const char_t* src, size_t dstSize)
+		{
+			wmemcpy(dst, src, dstSize);
+		}
+
+		static void move(char_t* dst, const char_t* src, size_t dstSize)
+		{
+			wmemmove(dst, src, dstSize);
+		}
+
+		static void assing(char_t* dst, char_t c, size_t count)
+		{
+			wmemset(dst, c, count);
 		}
 	};
 }
